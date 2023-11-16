c7e10aa3c64911356ac66bd0185b3af1031c022b
Jewls135 <juliansw@usca.edu>
2023-11-16 18:49:51 -0500

    Changed image, updated homepage and main css

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,15 @@
+// Starting effect
 $(document).ready(function(){
-    $('body').hide();
-    $('body').fadeIn(2000);
+    $('div').hide();
+    $('.mainlogo').fadeIn(2000);
+    $('.continue').fadeIn(2000);
+});
+// Functions below
+
+
+// Events below
+$('#continueButton').click(function(){
+    $('.continue').fadeOut(1000);
+    $('.mainlogo').fadeOut(1000);
+    $('.loginform').fadeIn(2000);
 });

92b28824656d7bcce8b3ef982ef42890d5adee89
Jewls135 <juliansw@usca.edu>
2023-11-16 19:55:20 -0500

    Added input elements, changed style

--- js/homepage.js
+++ js/homepage.js
@@ -11,5 +11,8 @@ $(document).ready(function(){
 $('#continueButton').click(function(){
     $('.continue').fadeOut(1000);
     $('.mainlogo').fadeOut(1000);
-    $('.loginform').fadeIn(2000);
+    
+    setTimeout(function(){ // Waiting 1.4 seconds before fading the form in
+        $('.loginform').fadeIn(1000);
+    }, 1400)
 });

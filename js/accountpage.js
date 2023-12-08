fc94db91e6a0416bb3e87c4509e6217b8f198e35
kgolston13 <kgolston13@gmail.com>
2023-12-08 12:49:57 -0500

    Update account page with transaction history display

--- js/accountpage.js
+++ js/accountpage.js
@@ -1,14 +1,14 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBRzL4hYESxYOmG6qBchI2hRGi4XI3rX-A",
-    authDomain: "banking-jlk.firebaseapp.com",
-    projectId: "banking-jlk",
-    storageBucket: "banking-jlk.appspot.com",
-    messagingSenderId: "843171081265",
-    appId: "1:843171081265:web:508c3dc3ab8f91e5b87910",
-    measurementId: "G-C0HD55FR3R"
+  apiKey: "AIzaSyBRzL4hYESxYOmG6qBchI2hRGi4XI3rX-A",
+  authDomain: "banking-jlk.firebaseapp.com",
+  projectId: "banking-jlk",
+  storageBucket: "banking-jlk.appspot.com",
+  messagingSenderId: "843171081265",
+  appId: "1:843171081265:web:508c3dc3ab8f91e5b87910",
+  measurementId: "G-C0HD55FR3R"
 };
-  
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -22,70 +22,63 @@ function handleError(error) {
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     currentUser = user;
+
+    const userCardRef = db.collection('userCard').doc(currentUser.uid);
+
+    userCardRef.get().then((cardDoc) => {
+      if (cardDoc.exists) {
+        const userId = cardDoc.id; // Using document ID as UserID
+
+        if (userId) {
+          // Continue with the rest of your code to retrieve and display the card number
+          const cardNumber = cardDoc.id; // Get the card number from the document ID
+          document.getElementById('cardNumber').value = cardNumber;
+        } else {
+          console.log('No UserID found in userCard for this user!');
+        }
+      } else {
+        console.log('No card document found for this user!');
+      }
+    }).catch((cardError) => {
+      handleError(cardError);
+    });
+
     const userDataRef = db.collection('userData').doc(currentUser.uid);
 
     userDataRef.get().then((doc) => {
       if (doc.exists) {
         const userData = doc.data();
-        const userID = userData.userID;
-    
-        // Get userCard document corresponding to the userID
-        const userCardRef = db.collection('userCard').doc(userID);
-    
-        userCardRef.get().then((cardDoc) => {
-          if (cardDoc.exists) {
-            const cardNumber = cardDoc.id; // Get the card number from the document ID
-            document.getElementById('cardNumber').value = cardNumber;
-          } else {
-            console.log('No card document found for this user!');
-          }
-        }).catch((cardError) => {
-          handleError(cardError); // Error handling for getting card data
-        });
 
-        if (!Array.isArray(transactionHistory)) {
-          transactionHistory = [transactionHistory];
-        }
-
+        const transactionHistory = userData.transactionHistory || []; // Ensure it's an array
         const currentBalance = userData.balance || 0;
-        document.getElementById('balance').value = currentBalance;
+
+        document.getElementById('balance').value = currentBalance; // Display the current balance
 
         const table = document.getElementById('transactionTable');
-        table.innerHTML = '<tr><th>Type</th><th>Amount</th></tr>';
 
-        transactionHistory.forEach((transaction) => {
+        // Loop through transactionHistory map and populate the table
+        for (const [key, value] of Object.entries(transactionHistory)) {
           const row = table.insertRow(-1);
-          const cellType = row.insertCell(0);
-          const cellAmount = row.insertCell(1);
-        
-          if (typeof transaction === 'string') {
-            const details = transaction.split(':');
-            if (details.length === 2) {
-              cellType.innerHTML = details[0];
-              cellAmount.innerHTML = details[1];
-            } else {
-              console.log('Invalid transaction format:', transaction);
-              cellType.innerHTML = 'Unknown';
-              cellAmount.innerHTML = 'N/A';
-            }
-          } else if (typeof transaction === 'object' && transaction.type === 'initialDeposit') {
-            cellType.innerHTML = 'Initial Deposit';
-            cellAmount.innerHTML = transaction.amount || 'N/A'; // Display amount or 'N/A'
-          } else {
-            console.log('Invalid transaction:', transaction);
-            cellType.innerHTML = 'Unknown';
-            cellAmount.innerHTML = 'N/A';
-          }
-        });
+          const cellDate = row.insertCell(0);
+          const cellType = row.insertCell(1);
+          const cellAmount = row.insertCell(2);
+
+          // Accessing field (date), type, and value fields from each transaction object
+          cellDate.innerHTML = key || 'Unknown Date'; // Display the date
+          cellType.innerHTML = value >= 0 ? 'Deposit' : 'Withdrawal'; // Display the type
+          cellAmount.innerHTML = Math.abs(value) || 'N/A'; // Display the absolute value as the amount
+        }
+
       } else {
         console.log('No such document!');
       }
     }).catch((error) => {
-      handleError(error); // Error handling for getting user data
+      handleError(error);
     });
+
   } else {
     console.log("User is not logged in");
-    window.location.href = "loginpage.html"
+    window.location.href = "loginpage.html";
   }
 });
 
@@ -100,7 +93,6 @@ function toggleForm(formId) {
     }
   }
 }
-
 
 // Event listener for transaction history button
 document.getElementById('history').addEventListener('click', function () {
